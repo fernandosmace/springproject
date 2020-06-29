43784b5a8899609b62a72a82d814e980f31cf4a4
Fernando Macedo <fsoares0055@gmail.com>
2020-06-29 01:22:10 -0300

    Configurando banco H2 e mapeamento da persistencia com JPA

--- src/main/java/com/springproject/domain/Cartao.java
+++ src/main/java/com/springproject/domain/Cartao.java
@@ -3,9 +3,17 @@ package com.springproject.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cartao implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private int numero;
 	private Date vencimento;
